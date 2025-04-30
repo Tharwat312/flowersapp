a8b71a6aa569507a4a93fc114cec620329a94ba0
tharwat312 <tharwata312@gmail.com>
2025-04-30 22:12:10 +0300

    Fix Typescript Errors

--- src/lib/types/next-auth.d.ts
+++ src/lib/types/next-auth.d.ts
@@ -1,57 +1,4 @@
-// declare module "next-auth" {
-//     interface User {
-//         "user": {
-//             "firstName": string,
-//             "lastName": string,
-//             "email": string,
-//             "gender": "male" | "female",
-//             "phone": string,
-//             "photo": string | null,
-//             "role": "user" | "admin",
-//             "wishlist": [],
-//             "addresses": [],
-//         } & DataBaseRecords,
-//         "token": string
-//     }
-//     interface Session extends Omit<User, "token"> {
 
-//     }
-// }
-
-// import NextAuth from "next-auth";
-
-// declare module "next-auth" {
-//     interface Session {
-//         user: {
-//             id: string;
-//             email: string;
-//             name?: string;
-//             "firstName": string,
-//             "lastName": string,
-//             "email": string,
-//             "gender": "male" | "female",
-//             "phone": string,
-//             "photo": string | null,
-//             "role": "user" | "admin",
-//             "wishlist": [],
-//             "addresses": [],
-
-//             // any other custom fields
-//         };
-//     }
-// }
-
-// declare module "next-auth/jwt" {
-//     interface JWT {
-//         user?: {
-//             id: string;
-//             email: string;
-//             token?: string;
-//         };
-//     }
-// }
-
-// types/next-auth.d.ts
 import { DefaultSession, DefaultUser } from "next-auth";
 import { JWT as DefaultJWT } from "next-auth/jwt";
 
@@ -67,8 +14,8 @@ declare module "next-auth" {
             phone: string;
             photo: string | null;
             role: "user" | "admin";
-            wishlist: string[]; // Replace 'any' with proper type if possible
-            addresses: string[]; // Replace 'any' with proper type if possible
+            wishlist: string[];
+            addresses: string[];
         };
     }
 
@@ -85,13 +32,11 @@ declare module "next-auth/jwt" {
     }
 }
 
-// Shared API response type
 export interface APIResponse<T> {
     data?: T;
     error?: string;
 }
 
-// Login response type
 export interface LoginResponse {
     user: Session["user"];
     token: string;
